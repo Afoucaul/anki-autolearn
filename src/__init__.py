e229ca2467a909f20ce3341d9bcbdcf3f347dca6
armand <armand.foucault@telecom-bretagne.eu>
2019-03-05 22:42:00 +0100

    Working on customizable interval time

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ import aqt
 from aqt.qt import *
 
 
-POP_EVERY_MIN = 5
+POP_EVERY_MS = 5 * 60 * 1000
 
 
 
@@ -17,22 +17,50 @@ def restore_mw():
     aqt.mw.setFocus()
 
 
-def hook():
-    hide_mw()
-    QTimer.singleShot(POP_EVERY_MIN * 60000, restore_mw)
+class Hook:
+    def __init__(self, delay=POP_EVERY_MS):
+        self.delay = delay
 
-
-hooks.addHook('cardAnswered', hook)
-
-
+    def __call__(self):
+        hide_mw()
+        QTimer.singleShot(self.delay, restore_mw)
 
 
 class ScheduleDialog(QDialog):
     def __init__(self, mw):
         super().__init__(mw)
+
+        layout = QFormLayout(self)
+
+        self.spinMinutes = QSpinBox(self)
+        self.spinSeconds = QSpinBox(self)
+        self.okButton = QPushButton("OK")
+
+        layout.addRow(QLabel("Minutes"), self.spinMinutes)
+        layout.addRow(QLabel("Seconds"), self.spinSeconds)
+        layout.addRow(self.okButton)
+
+        self.spinMinutes.setValue(5)
+        self.spinSeconds.setValue(0)
+        self.okButton.clicked.connect(self.onOk)
+
         self.show()
+
+    def onOk(self):
+        minutes = self.spinMinutes.value()
+        seconds = self.spinSeconds.value()
+        setupHook((minutes * 60 + seconds) * 1000)
+        print("Setup hook with", (minutes * 60 + seconds) * 1000, "ms")
+
+
+hook = Hook()
+
+hooks.addHook('cardAnswered', hook)
 
 
 action = QAction("Autolearn schedule", aqt.mw)
 action.triggered.connect(lambda: ScheduleDialog(aqt.mw))
 aqt.mw.form.menuTools.addAction(action)
+
+
+setupHook()
